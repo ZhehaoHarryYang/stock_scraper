47f70970aa5595c7a6d0b3d46102ec865ddc3bfe
Harry Cat <harryyang.eng@gmail.com>
2024-07-31 14:14:48 +0800

    add a new py for news and details

---
+++ Scraping/SingleStock_News_Detail.py
@@ -0,0 +1,21 @@
+from init_path import *  # Import the initialization script
+
+from models.mongo_connection import get_collection, get_database
+from SingleStock_Info import get_stock_info, get_stock_news
+
+def update_stock_info(symbol):
+    db = get_database()
+    collection_detail = get_collection(db, 'stock_details')
+    detailInfo = get_stock_info(symbol)
+    detailInfo['newsList'] = get_stock_news(symbol)
+
+    if collection_detail.find_one({'symbol': symbol}):
+        collection_detail.delete_one({'symbol': symbol})
+        collection_detail.insert_one(detailInfo)
+        print(f"Details with symbol {symbol} updated.")
+    else:
+        collection_detail.insert_one(detailInfo)
+        print(f"Details with symbol {symbol} inserted.")
+
+
+update_stock_info('AAPL')
