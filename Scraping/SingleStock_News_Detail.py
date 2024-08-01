708a42f7ceda7d6bee434803493e42cace3d306b
Harry Cat <harryyang.eng@gmail.com>
2024-08-01 17:17:08 +0800

    update detail logic to handle empty data

--- Scraping/SingleStock_News_Detail.py
+++ Scraping/SingleStock_News_Detail.py
@@ -2,35 +2,35 @@ from init_path import *  # Import the initialization script
 
 from models.mongo_connection import get_collection, get_database
 from SingleStock_Info import get_stock_info, get_stock_news
-from tenacity import retry, wait_exponential, stop_after_attempt
 
-
-# Retry logic with exponential backoff
-@retry(wait=wait_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(5))
 def fetch_stock_info(symbol):
     detailInfo = get_stock_info(symbol)
-    if not detailInfo['name']:  # Check if the detailInfo is empty or None
-        raise ValueError(f"Received empty data for symbol {symbol}")
     detailInfo['newsList'] = get_stock_news(symbol)
+    if not detailInfo.get('name'):  # Check if the detailInfo is empty or None
+        return None
     return detailInfo
 
 def get_stock_details(symbol):
     db = get_database()
     collection_detail = get_collection(db, 'stock_details')
+
+    # First attempt to fetch stock info
+    detailInfo = fetch_stock_info(symbol)
     
-    try:
+    if detailInfo is None:  # If data is empty, retry once
+        print(f"Received empty data for symbol {symbol}. Retrying...")
         detailInfo = fetch_stock_info(symbol)
-        
-        if collection_detail.find_one({'symbol': symbol}):
-            collection_detail.delete_one({'symbol': symbol})
-            collection_detail.insert_one(detailInfo)
-            print(f"Details with symbol {symbol} updated.")
-        else:
-            collection_detail.insert_one(detailInfo)
-            print(f"Details with symbol {symbol} inserted.")
-    
-    except Exception as e:
-        print(f"An error occurred while updating stock info for {symbol}: {e}")
+        return
+
+    if collection_detail.find_one({'symbol': symbol}):
+        collection_detail.update_one(
+            {'symbol': symbol},  # Query to find the document
+            {'$set': detailInfo},  # Update the document with detailInfo
+        )
+        print(f"Details with symbol {symbol} updated.")
+    else:
+        collection_detail.insert_one(detailInfo)
+        print(f"Details with symbol {symbol} inserted.")
 
 # Test the function
-# update_stock_info('AAPL')
+# get_stock_details('GE')
