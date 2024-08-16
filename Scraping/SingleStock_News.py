27ccd05d7a136559072e6342bb8a185e3213d0c2
Harry Cat <harryyang.eng@gmail.com>
2024-08-16 21:22:24 +0800

    modified the news scraping to handle nothing found and print notes

--- Scraping/SingleStock_News.py
+++ Scraping/SingleStock_News.py
@@ -10,20 +10,22 @@ def fetch_stock_news(symbol):
     """Fetch stock news for a given symbol."""
     newsList = get_stock_news(symbol)
     if not newsList:  # Check if the newsList is empty
-        return None
+        return []
     return newsList
 
 def update_stock_news(symbol):
     newsList = fetch_stock_news(symbol)
-    """Update or insert stock news into the database."""
-    if collection_news.find_one({'symbol': symbol}):
-        collection_news.update_one(
-            {'symbol': symbol},  # Query to find the document
-            {'$set': {'newsList': newsList}},  # Update the document with newsList
-        )
-        print(f"News for symbol {symbol} updated.")
-    else:
-        collection_news.insert_one({'symbol': symbol, 'newsList': newsList})
-        print(f"News for symbol {symbol} inserted.")
+    if newsList:
+        if collection_news.find_one({'symbol': symbol}):
+            collection_news.update_one(
+                {'symbol': symbol},  # Query to find the document
+                {'$set': {'newsList': newsList}},  # Update the document with newsList
+            )
+            print(f"{len(newsList)} News for symbol {symbol} updated.")
+        else:
+            collection_news.insert_one({'symbol': symbol, 'newsList': newsList})
+            print(f"{len(newsList)} News for symbol {symbol} just got inserted.")
+    else: 
+        print(f"No News for symbol {symbol} found and nothing insert or update")
 
 # update_stock_news('NTTYY')
